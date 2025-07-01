a6a353b013e6feda34f0c045c3c703b6e3fce91a
Mucci <121899648+MucciDev@users.noreply.github.com>
2025-07-01 11:18:16 +0100

    Update ffmpeg.ts

--- src/ffmpeg.ts
+++ src/ffmpeg.ts
@@ -1,69 +1,43 @@
-import { spawn } from "child_process";
-import { createReadStream } from 'fs';
-import { pipeline } from 'stream/promises';
-import { resolve } from 'path';
+import { exec } from "child_process";
+import fs from 'fs/promises';
+import * as path from 'path';
 
-const sh = (command, args) => {
+const sh = (command: string): Promise<{ stdout: string, stderr: string }> => {
   return new Promise((resolve, reject) => {
-    const process = spawn(command, args);
-    let stdout = '';
-    let stderr = '';
-
-    process.stdout.on('data', data => {
-      stdout += data;
-    });
-
-    process.stderr.on('data', data => {
-      stderr += data;
-    });
-
-    process.on('close', code => {
-      if (code !== 0) {
-        reject(new Error(`Process exited with code ${code}: ${stderr}`));
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
       } else {
         resolve({ stdout, stderr });
       }
     });
-
-    process.on('error', err => {
-      reject(err);
-    });
   });
 };
 
-const joinPath = (...paths) => {
-  return resolve(__dirname, '../', ...paths);
-};
+const joinPath = (...paths: string[]) => {
+  return path.join(__dirname, '../', ...paths);
+}
 
-export const generateGIF = async (gif, identifier) => {
+export const generateGIF = async (gif: string, identifier: string): Promise<Buffer> => {
   try {
-    const textPath = joinPath(`_temp/texts/${identifier}.png`);
+    const textPath = joinPath(`_temp/texts/${identifier}-text.png`);
     const gifPath = joinPath(`assets/${gif}.gif`);
+    const destinationPath = joinPath(`_temp/${identifier}.gif`);
     const palettePath = joinPath(`_temp/${identifier}-palette.png`);
 
-    await sh('ffmpeg', [
-      '-i', textPath,
-      '-i', gifPath,
-      '-filter_complex', `[0:v]scale=700:-1[v0]; [1:v]scale=700:-1[v1]; [v0][v1]vstack=inputs=2,fps=15,palettegen[v]`,
-      '-map', '[v]',
-      '-y', palettePath
-    ]);
+    await sh(
+        `ffmpeg -i "${textPath}" -i "${gifPath}" -filter_complex "[0:v]scale=700:-1[v0]; [1:v]scale=700:-1[v1]; [v0][v1]vstack=inputs=2,fps=15,palettegen[v]" -map "[v]" -y "${palettePath}"`
+    )
 
-    const destinationPath = joinPath(`_temp/${identifier}.gif`);
-
-    await sh('ffmpeg', [
-      '-i', textPath,
-      '-i', gifPath,
-      '-i', palettePath,
-      '-filter_complex', `[0:v]scale=700:-1[v0]; [1:v]scale=700:-1[v1]; [v0][v1]vstack=inputs=2,fps=15[v]; [v][2:v]paletteuse[v_optimized]`,
-      '-map', '[v_optimized]',
-      '-fs', '15M',
-      '-b:v', '10M',
-      '-loop', '0',
-      destinationPath
-    ]);
+    await sh(
+        `ffmpeg -i "${textPath}" -i "${gifPath}" -i "${palettePath}" -filter_complex "[0:v]scale=700:-1[v0]; [1:v]scale=700:-1[v1]; [v0][v1]vstack=inputs=2,fps=15[v]; [v][2:v]paletteuse[v_optimized]" -map "[v_optimized]" -fs 9M -b:v 10M -loop 0 "${destinationPath}"`
+    )
 
     const image = await fs.readFile(destinationPath);
+
+    await fs.unlink(textPath);
+    await fs.unlink(palettePath);
+    await fs.unlink(destinationPath);
 
     return image;
   } catch (err) {
